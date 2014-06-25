da249b3eb63f3cbc0ca986d80b7f3f69c502d6b1
Jameson Proctor <jameson@jp1971.com>
2014-06-24 21:48:01 -0400

    Abstracting AJAX functions. Cleaning up whitespace.

--- js/yun/ctrl.js
+++ js/yun/ctrl.js
@@ -19,59 +19,35 @@ define( ['jquery', 'keypress'], function ( $, keypress ) {
 
 		// Click functions
 		$( 'button' ).click( function() {
-
 			$( 'button' ).not( this ).removeClass( 'active' );
 			$( this ).addClass( 'active' );
 		} );
 
 		$( 'button.forward' ).click( function() {
-
 			_forward();
 		} );
 
-		$('button.left').click(function(){
-
+		$( 'button.left' ).click( function() {
 			_left();			
-		});
+		} );
 
 		$( 'button.stop' ).click( function() {
-
-			$.ajax( {
-				type: 'POST',
-				url: '/arduino/2'
-			} )
-			.done( function( text ) {
-				console.log( text );
-			} );
+			_stop();
 		});
 
 		$( 'button.right' ).click( function() {
-
-			$.ajax( {
-				type: 'POST',
-				url: '/arduino/3'
-			} )
-			.done( function( text ) {
-				console.log( text );
-			} );
+			right();
 		});
 
 		$( 'button.reverse' ).click( function() {
-
-			$.ajax( {
-				type: 'POST',
-				url: '/arduino/4'
-			} )
-			.done( function( text ) {
-				console.log( text );
-			} );
+			_reverse();
 		});
 
-		var my_combos = listener.register_many([
+		var my_combos = listener.register_many( [
 		    {
 		        'keys'          : 'space',
 		        'on_keydown'    : function() {
-
+		        	_stop();
 		        },
 		        'prevent_defaut': true
 		    },
@@ -92,18 +68,18 @@ define( ['jquery', 'keypress'], function ( $, keypress ) {
 		    {
 		        'keys'          : 'right',
 		        'on_keydown'    : function() {
-		            console.log('You pressed right.');
+		            _right();
 		        },
 		        'prevent_defaut': true
 		    },
 		    {
 		        'keys'          : 'down',
 		        'on_keydown'    : function() {
-		            console.log('You pressed down.');
+		            _reverse();
 		        },
 		        'prevent_defaut': true
 		    }
-		]);
+		] );
 	}
 
 	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
@@ -132,4 +108,37 @@ define( ['jquery', 'keypress'], function ( $, keypress ) {
 
 	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+	function _stop() {
+		$.ajax( {
+			type: 'POST',
+			url: '/arduino/2'
+		} )
+		.done( function( text ) {
+			console.log( text );
+		} );
+	}
+
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+	function _right() {
+		$.ajax( {
+			type: 'POST',
+			url: '/arduino/3'
+		} )
+		.done( function( text ) {
+			console.log( text );
+		} );
+	}
+
+	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+	function _reverse() {
+		$.ajax( {
+			type: 'POST',
+			url: '/arduino/4'
+		} )
+		.done( function( text ) {
+			console.log( text );
+		} );
+	}
 });	
